4f02900fb22aa532e90749be073370e64d472e1c
yupin <83113114+Yupinnu@users.noreply.github.com>
2021-06-10 14:54:06 +0700

    update

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ from forms import LoginForm, RegisterForm, CreatePostForm, CommentForm
 from flask_gravatar import Gravatar
 import os
 
+SECRET_KEY = os.urandom(32)
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+app.config['SECRET_KEY'] = SECRET_KEY
 ckeditor = CKEditor(app)
 Bootstrap(app)
 gravatar = Gravatar(app, size=100, rating='g', default='retro', force_default=False, force_lower=False, use_ssl=False, base_url=None)
